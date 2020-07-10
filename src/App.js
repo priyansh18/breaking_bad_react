947cc26c865c707cf85e1b6eb78cb88c2614e730
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-07-10 18:22:53 +0530

    Fetching data from API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Axios from "axios";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState([]);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await Axios(`https://www.breakingbadapi.com/api/characters`);
+
+      console.log(result.data);
+    };
+
+
+    fetchItems()
+  },[]);
+
   return (
     <div className="App">
       <Header />
