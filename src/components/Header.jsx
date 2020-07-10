96e80980be3a0f47f45446b5093117fb98d995dc
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-07-10 18:09:07 +0530

    Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import logo from "../img/logo.png";
+
+const Header = () => {
+  return (
+    <header className="center">
+      <img src={logo} alt="" />
+    </header>
+  );
+};
+
+export default Header;
