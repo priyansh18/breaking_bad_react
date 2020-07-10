ca52e5b4a04c8f60735fa4215eaf7d5427d160e5
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-07-10 20:20:08 +0530

    CharacterGrid Component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,28 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Axios from "axios";
+import CharacterGrid from "./components/CharacterGrid";
 
 function App() {
   const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchItems = async () => {
       const result = await Axios(`https://www.breakingbadapi.com/api/characters`);
 
       console.log(result.data);
+      setItems(result.data);
+      setIsLoading(false);
     };
 
-
-    fetchItems()
-  },[]);
+    fetchItems();
+  }, []);
 
   return (
-    <div className="App">
+    <div className="container">
       <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 }
